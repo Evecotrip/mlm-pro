59d62cfca9d073287d4ce3021e6c09ca38627d47
Aryan Jagarwal <ajbaggar@gmail.com>
2025-12-02 12:58:29 +0530

    logo size

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -16,22 +16,22 @@ export default function Logo({ size = 'md', subtitle, className = '' }: LogoProp
   // Size configurations
   const sizeConfig = {
     sm: {
-      container: 'w-10 h-10 sm:w-12 sm:h-12 -ml-2',
+      container: 'w-14 h-14 sm:w-16 sm:h-16 -ml-3',
       text: 'text-xl sm:text-2xl',
       subtitle: 'text-xs sm:text-sm',
     },
     md: {
-      container: 'w-12 h-12 sm:w-16 sm:h-16 -ml-2',
+      container: 'w-16 h-16 sm:w-20 sm:h-20 -ml-3',
       text: 'text-2xl sm:text-3xl',
       subtitle: 'text-sm sm:text-base',
     },
     lg: {
-      container: 'w-20 h-20 sm:w-24 sm:h-24 -ml-3',
+      container: 'w-24 h-24 sm:w-28 sm:h-28 -ml-4',
       text: 'text-3xl sm:text-4xl',
       subtitle: 'text-base sm:text-lg',
     },
     xl: {
-      container: 'w-24 h-24 sm:w-28 sm:h-28 -ml-3',
+      container: 'w-28 h-28 sm:w-32 sm:h-32 -ml-4',
       text: 'text-4xl sm:text-5xl',
       subtitle: 'text-lg sm:text-xl',
     },
